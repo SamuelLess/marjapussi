c22bae00a1a92854e9fd9fdaee798d18b66a063f
Samuel <Samuel@LMpost.de>
2022-08-06 00:41:13 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="marjapussi",
+    version="0.1",
+    author="Samuel Leßmann",
+    author_email="Samuel@LMpost.de",
+    description="Python Implementation of MarjaPussi.",
+    long_description=long_description,
+    packages=["marjapussi"],
+    python_requires='>=3.10',
+)
