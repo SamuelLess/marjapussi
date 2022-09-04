a6ea6a3afd94f090d5b971fb3d47abe74c1bd915
Samuel <Samuel@LMpost.de>
2022-09-04 18:33:06 +0200

    version increment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setuptools.setup(
     name="marjapussi",
-    version="0.1.1",
+    version="0.1.2",
     author="Samuel Leßmann",
     author_email="Samuel@LMpost.de",
     description="Python Implementation of MarjaPussi.",
