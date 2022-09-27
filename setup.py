37af812a4f1468901ebaef26822819e4d6a40dbf
Samuel <Samuel@LMpost.de>
2022-09-27 18:05:40 +0200

    fix wrong card force

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 import setuptools
+import marjapussi
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
 setuptools.setup(
     name="marjapussi",
-    version="0.1.9",
-    author="Samuel Leßmann",
+    version=marjapussi.__version__,
+    author=marjapussi.__author__,
     author_email="Samuel@LMpost.de",
     description="Python Implementation of MarjaPussi.",
     long_description=long_description,
